bb71c8a883c022367dd2f9e509f61d5c11967425
VirtFunc <admin@virtfunc.com>
2025-08-16 16:23:07 -0700

    add comments, rearrange worker.js event handlers, change some formatting, add note about firefox. merge loader.js into worker.js

--- init.js
+++ init.js
@@ -1,10 +1,162 @@
-function patchButtonState(string, disabled) {
-  document.getElementById("run-patch").disabled = disabled;
-  document.getElementById("run-patch").innerText = string;
-}
-
+// gVar -> my prof is malding rn.
 var patchMapping = {};
 
+//load the worker (which loads the wasm + js wrapper)
+function init() {
+  var status = document.querySelector("#status");
+  var output = document.getElementById("output");
+  var worker = new Worker("worker.js");
+  // inform the user that the worker is not ready. loading from network.
+  patchButtonState("Loading...", true);
+  worker.onmessage = function (e) {
+    switch (e.data.type) {
+      case "wasmProgress":
+        //console.log(`Wasm loading progress: ${e.data.progress}%`);
+        const progressBar = document.getElementById("wasm-progress-bar");
+        const progressText = document.getElementById("wasm-progress-text");
+
+        if (progressBar && progressText) {
+          progressBar.style.width = e.data.progress + "%";
+          progressText.textContent = `Loading Wasm: ${e.data.progress}%`;
+
+          // Hide the progress elements when complete
+          if (e.data.progress >= 100) {
+            progressBar.parentElement.style.display = "none";
+            progressText.style.display = "none";
+          }
+        }
+        break;
+
+      case "ready":
+        // enable the patch button
+        patchButtonState("Patch", false);
+        break;
+      case "error":
+        patchButtonState("Patch", false);
+        status.innerHTML = e.data.text;
+        break;
+      case "stdout":
+      case "stderr":
+        output.innerHTML += e.data.text + "\n";
+        break;
+      case "complete":
+        // when we are done, extract the output file from the worker, save it to the user's machine
+        // then reset the system for the next patch by enabling the patch button.
+        var blob = new Blob([e.data.data], {
+          type: "application/octet-stream",
+        });
+        // get file name from the input rom file name and add _patched to the filename before extension
+        var inputRomName = document.getElementById("input-rom").files[0].name;
+        var outputRomName = inputRomName.replace(/(\.[^/.]+)+$/, "_patched$1");
+        var url = URL.createObjectURL(blob);
+        var a = document.createElement("a");
+        a.href = url;
+        a.download = outputRomName;
+        a.click();
+        URL.revokeObjectURL(url);
+        patchButtonState("Patch", false);
+        break;
+    }
+  };
+
+  // pull the patches json.
+  // if you dont like this, feel free to block this request with your browser, and you can supply ur own patches.
+  // but as you can see later, it will always show the contents of the pending patches giving the user a chance
+  // to audit it as they see fit.
+  fetch("patches.json")
+    .then((response) => response.json())
+    .then((data) => {
+      buildPatches(data.patches);
+      refreshPatches();
+    })
+    .catch((error) => {
+      console.error("Error loading patches:", error);
+      status.innerHTML = "Error loading patches.json";
+    });
+
+
+  // EVENT LISTENERS
+    // patch button listener
+  document.getElementById("run-patch").addEventListener("click", function () {
+    output.innerText = "";
+    status.innerText = "";
+    var inputRom = document.getElementById("input-rom").files[0];
+    var patchesTxt = document.getElementById("patches-txt").innerText;
+    if (inputRom) {
+      var reader = new FileReader();
+      reader.onload = function (e) {
+        var inputRomArray = new Uint8Array(e.target.result);
+        patchButtonState("Patching...", true);
+        worker.postMessage({
+          type: "runPatch",
+          inputRomArray: inputRomArray,
+          patchesTxt: patchesTxt,
+        });
+      };
+      reader.readAsArrayBuffer(inputRom);
+    } else {
+      alert("Please select an INPUT.ROM file.");
+    }
+  });
+
+    // patch checkbox listener -> update patches
+  document
+    .getElementById("patches-selector")
+    .addEventListener("click", function (e) {
+      if (e.target.type === "checkbox") {
+        refreshPatches();
+      }
+  });
+    // when custom patches textbox is updated, refresh the patches.
+  document
+    .querySelector('textarea[data-patchname="custom"]')
+    .addEventListener("input", function () {
+      //console.log("Custom patches updated");
+      refreshPatches();
+  });
+    // drag file listener
+  document
+  .addEventListener("dragover", (e) => {
+    e.preventDefault();
+    if (e.dataTransfer.types.includes("Files")) {
+      document.body.style.backgroundColor = "#0000e1"; // Change to whatever color you want
+    }
+  });
+    // watch for leaving the drop zone
+  document
+    .addEventListener("dragleave", (e) => {
+      document.body.style.backgroundColor = ""; // Reset to original color
+  });
+    // drop file listener
+  document
+    .addEventListener("drop", (e) => {
+      e.preventDefault();
+      document.body.style.backgroundColor = "";
+      document.getElementById("input-rom").files = e.dataTransfer.files;
+  });
+
+
+}
+function refreshPatches() {
+  //console.log("Refreshing patches...");
+  patchMapping["custom"] = document.querySelector(
+    'textarea[data-patchname="custom"]',
+  ).value;
+  var patchesTxt = "";
+  //loop through all the checkboxes with data-patchname, and add the corresponding patch to patchesTxt
+  //patch name is a variable that is the same as the data-patchname attribute
+  //so we can use it to avoid hardcoding the patch names in our loop.
+  var checkboxes = document.querySelectorAll('input[type="checkbox"]');
+  for (var i = 0; i < checkboxes.length; i++) {
+    var patchName = checkboxes[i].getAttribute("data-patchname");
+    if (checkboxes[i].checked && patchMapping[patchName]) {
+      patchesTxt += patchMapping[patchName];
+    }
+  }
+  document.getElementById("patches-txt").innerText = patchesTxt;
+}
+
+// given some fetched patches, build the UI elements for each patch
 function buildPatches(patches) {
   const container = document.getElementById("preset-patches");
   patches.forEach((patch) => {
@@ -29,136 +181,7 @@ function buildPatches(patches) {
   });
 }
 
-function init() {
-  var status = document.querySelector("#status");
-  var output = document.getElementById("output");
-  var worker = new Worker("worker.js");
-  // inform the user that the worker is not ready. loading from network.
-  patchButtonState("Loading...", true);
-  worker.onmessage = function (e) {
-    switch (e.data.type) {
-      case "wasmProgress":
-        const progressBar = document.getElementById("wasm-progress-bar");
-        const progressText = document.getElementById("wasm-progress-text");
-
-        if (progressBar && progressText) {
-          progressBar.style.width = e.data.progress + "%";
-          progressText.textContent = `Loading Wasm: ${e.data.progress}%`;
-
-          // Hide the progress elements when complete
-          if (e.data.progress === 100) {
-            progressBar.parentElement.style.display = "none";
-            progressText.style.display = "none";
-          }
-        }
-        break;
-
-      case "ready":
-        // enable the patch button
-        patchButtonState("Patch", false);
-        break;
-      case "error":
-        patchButtonState("Patch", false);
-        status.innerHTML = e.data.text;
-        break;
-      case "stdout":
-      case "stderr":
-        output.innerHTML += e.data.text + "\n";
-        break;
-      case "complete":
-        var blob = new Blob([e.data.data], {
-          type: "application/octet-stream",
-        });
-        //get file name from the input rom file name and add _patched to the filename before extension
-        var inputRomName = document.getElementById("input-rom").files[0].name;
-        var outputRomName = inputRomName.replace(/(\.[^/.]+)+$/, "_patched$1");
-        var url = URL.createObjectURL(blob);
-        var a = document.createElement("a");
-        a.href = url;
-        a.download = outputRomName;
-        a.click();
-        URL.revokeObjectURL(url);
-        patchButtonState("Patch", false);
-        break;
-    }
-  };
-
-  fetch("patches.json")
-    .then((response) => response.json())
-    .then((data) => {
-      buildPatches(data.patches);
-      refreshPatches();
-    })
-    .catch((error) => {
-      console.error("Error loading patches:", error);
-      status.innerHTML = "Error loading patches.json";
-    });
-
-  document.getElementById("run-patch").addEventListener("click", function () {
-    output.innerText = "";
-    status.innerText = "";
-    var inputRom = document.getElementById("input-rom").files[0];
-    var patchesTxt = document.getElementById("patches-txt").innerText;
-    if (inputRom) {
-      var reader = new FileReader();
-      reader.onload = function (e) {
-        var inputRomArray = new Uint8Array(e.target.result);
-        patchButtonState("Patching...", true);
-        worker.postMessage({
-          type: "runPatch",
-          inputRomArray: inputRomArray,
-          patchesTxt: patchesTxt,
-        });
-      };
-      reader.readAsArrayBuffer(inputRom);
-    } else {
-      alert("Please select an INPUT.ROM file.");
-    }
-  });
-  //when any checkbox is clicked, update the preview for patches.txt
-  document
-    .getElementById("patches-selector")
-    .addEventListener("click", function (e) {
-      if (e.target.type === "checkbox") {
-        refreshPatches();
-      }
-    });
+function patchButtonState(string, disabled) {
+  document.getElementById("run-patch").disabled = disabled;
+  document.getElementById("run-patch").innerText = string;
 }
-function refreshPatches() {
-  patchMapping["custom"] = document.querySelector(
-    'textarea[data-patchname="custom"]',
-  ).value;
-  var patchesTxt = "";
-  //loop through all the checkboxes with data-patchname, and add the corresponding patch to patchesTxt
-  //patch name is a variable that is the same as the data-patchname attribute
-  //so we can use it to avoid hardcoding the patch names in our loop.
-  var checkboxes = document.querySelectorAll('input[type="checkbox"]');
-  for (var i = 0; i < checkboxes.length; i++) {
-    var patchName = checkboxes[i].getAttribute("data-patchname");
-    if (checkboxes[i].checked && patchMapping[patchName]) {
-      patchesTxt += patchMapping[patchName];
-    }
-  }
-  document.getElementById("patches-txt").innerText = patchesTxt;
-}
-
-// EVENT LISTENERS
-document.addEventListener("dragover", (e) => {
-  e.preventDefault();
-  if (e.dataTransfer.types.includes("Files")) {
-    document.body.style.backgroundColor = "#0000e1"; // Change to whatever color you want
-  }
-});
-document.addEventListener("dragleave", (e) => {
-  document.body.style.backgroundColor = ""; // Reset to original color
-});
-document.addEventListener("drop", (e) => {
-  e.preventDefault();
-  document.body.style.backgroundColor = "";
-  document.getElementById("input-rom").files = e.dataTransfer.files;
-});
-document
-  .querySelector('textarea[data-patchname="custom"]')
-  .addEventListener("input", function () {
-    refreshPatches();
-  });
--- worker.js
+++ worker.js
@@ -1,5 +1,374 @@
-importScripts("loader.js");
+//previously loader.js but now integrated into worker.js
+var Module = {};
+function Loader(config) {
+  function webAssemblySupported() {
+    return typeof WebAssembly !== "undefined";
+  }
 
+  function canLoad() {
+    return webAssemblySupported();
+  }
+
+  // Set default state handler functions and create canvases if needed
+  if (config.containerElements !== undefined) {
+    config.showError =
+      config.showError ||
+      function (errorText, container) {
+        removeChildren(container);
+        var errorTextElement = document.createElement("text");
+        errorTextElement.className = "Error";
+        errorTextElement.innerHTML = errorText;
+        return errorTextElement;
+      };
+
+    config.showLoader =
+      config.showLoader ||
+      function (loadingState, container) {
+        removeChildren(container);
+        var loadingText = document.createElement("text");
+        loadingText.className = "Loading";
+        loadingText.innerHTML = "<p><center> ${loadingState}...</center><p>";
+        return loadingText;
+      };
+
+    config.showExit =
+      config.showExit ||
+      function (crashed, exitCode, container) {
+        if (!crashed) return undefined;
+
+        removeChildren(container);
+        var fontSize = 54;
+        var crashSymbols = [
+          "\u{1F615}",
+          "\u{1F614}",
+          "\u{1F644}",
+          "\u{1F928}",
+          "\u{1F62C}",
+          "\u{1F915}",
+          "\u{2639}",
+          "\u{1F62E}",
+          "\u{1F61E}",
+          "\u{1F633}",
+        ];
+        var symbolIndex = Math.floor(Math.random() * crashSymbols.length);
+        var errorHtml = `<font size='${fontSize}'> ${crashSymbols[symbolIndex]} </font>`;
+        var errorElement = document.createElement("text");
+        errorElement.className = "Exit";
+        errorElement.innerHTML = errorHtml;
+        return errorElement;
+      };
+  }
+
+  config.restartMode = config.restartMode || "RestartOnCrash";
+
+  if (config.stdoutEnabled === undefined) config.stdoutEnabled = true;
+  if (config.stderrEnabled === undefined) config.stderrEnabled = true;
+
+  // Make sure config.path is defined and ends with "/" if needed
+  if (config.path === undefined) config.path = "";
+  if (config.path.length > 0 && !config.path.endsWith("/"))
+    config.path = config.path.concat("/");
+
+  if (config.environment === undefined) config.environment = {};
+
+  var pAPI = {};
+  pAPI.webAssemblySupported = webAssemblySupported();
+  pAPI.canLoad = canLoad();
+  pAPI.canLoadApplication = canLoad();
+  pAPI.status = undefined;
+  pAPI.loadModule = loadModule;
+
+  restartCount = 0;
+
+  function fetchResource(filePath) {
+    var fullPath = config.path + filePath;
+    return fetch(fullPath).then(function (response) {
+      if (!response.ok) {
+        self.error =
+          response.status + " " + response.statusText + " " + response.url;
+        setStatus("Error");
+        return Promise.reject(self.error);
+      } else {
+        return response;
+      }
+    });
+  }
+
+  function fetchText(filePath) {
+    return fetchResource(filePath).then(function (response) {
+      return response.text();
+    });
+  }
+
+  function fetchThenCompileWasm(response) {
+    return response.arrayBuffer().then(function (data) {
+      self.loaderSubState = "Compiling";
+      setStatus("Loading"); // trigger loaderSubState udpate
+      return WebAssembly.compile(data);
+    });
+  }
+
+  function fetchCompileWasm(filePath) {
+    return fetchResource(filePath).then(function (response) {
+      const contentLength = response.headers.get("Content-Length");
+      const total = parseInt(contentLength, 10);
+      let loaded = 0;
+      const reader = response.body.getReader();
+      const stream = new ReadableStream({
+        start(controller) {
+          function push() {
+            reader.read().then(({ done, value }) => {
+              if (done) {
+                controller.close();
+                self.postMessage({ type: "wasmProgress", progress: 100 }); // Send 100% completion
+                return;
+              }
+              loaded += value.length;
+              const progress = (loaded / total) * 100;
+              self.postMessage({
+                type: "wasmProgress",
+                progress: Math.round(progress),
+              }); // Send progress to main thread
+              controller.enqueue(value);
+              push();
+            });
+          }
+          push();
+        },
+      });
+      const response2 = new Response(stream, response);
+      if (typeof WebAssembly.compileStreaming !== "undefined") {
+        self.loaderSubState = "Downloading/Compiling";
+        setStatus("Loading");
+        return WebAssembly.compileStreaming(response2).catch(function (error) {
+          return fetchThenCompileWasm(response2);
+        });
+      } else return fetchThenCompileWasm(response2);
+    });
+  }
+
+  function loadModule(applicationName) {
+    // Loading in loader.js goes through four steps:
+    // 1) Check prerequisites
+    // 2) Download resources
+    // 3) Configure the emscripten Module object
+    // 4) Start the emcripten runtime, after which emscripten takes over
+
+    // Check for Wasm support, we dont care about WebGL; set error and return before downloading resources if missing
+    if (!webAssemblySupported()) {
+      self.error = "Error: WebAssembly is not supported";
+      setStatus("Error");
+      return;
+    }
+
+    // Continue waiting if loadModule() is called again
+    if (pAPI.status == "Loading") return;
+    self.loaderSubState = "Downloading";
+    setStatus("Loading");
+
+    // Fetch emscripten generated javascript runtime
+    var emscriptenModuleSource = undefined;
+    var emscriptenModuleSourcePromise = fetchText(applicationName + ".js").then(
+      function (source) {
+        emscriptenModuleSource = source;
+      },
+    );
+
+    // Fetch and compile wasm module
+    var wasmModule = undefined;
+    var wasmModulePromise = fetchCompileWasm(applicationName + ".wasm").then(
+      function (module) {
+        wasmModule = module;
+      },
+    );
+
+    // Wait for all resources ready
+    Promise.all([emscriptenModuleSourcePromise, wasmModulePromise])
+      .then(function () {
+        completeloadModule(applicationName, emscriptenModuleSource, wasmModule);
+      })
+      .catch(function (error) {
+        self.error = error;
+        setStatus("Error");
+      });
+  }
+
+  function completeloadModule(
+    applicationName,
+    emscriptenModuleSource,
+    wasmModule,
+  ) {
+    // The wasm binary has been compiled into a module during resource download,
+    // and is ready to be instantiated. Define the instantiateWasm callback which
+    // emscripten will call to create the instance.
+    Module.instantiateWasm = function (imports, successCallback) {
+      WebAssembly.instantiate(wasmModule, imports).then(
+        function (instance) {
+          successCallback(instance, wasmModule);
+        },
+        function (error) {
+          self.error = error;
+          setStatus("Error");
+        },
+      );
+      return {};
+    };
+
+    Module.locateFile =
+      Module.locateFile ||
+      function (filename) {
+        return config.path + filename;
+      };
+
+    // Attach status callbacks
+    Module.setStatus =
+      Module.setStatus ||
+      function (text) {
+        // Currently the only usable status update from this function
+        // is "Running..."
+        if (text.startsWith("Running")) setStatus("Running");
+      };
+    Module.monitorRunDependencies =
+      Module.monitorRunDependencies ||
+      function (left) {
+        //  console.log("monitorRunDependencies " + left)
+      };
+
+    // Attach standard out/err callbacks.
+    Module.print =
+      Module.print ||
+      function (text) {
+        if (config.stdoutEnabled) {
+          console.log(text);
+          const output = document.getElementById("output");
+          output.appendChild(document.createTextNode(text));
+          output.appendChild(document.createElement("br"));
+        }
+      };
+    Module.printErr =
+      Module.printErr ||
+      function (text) {
+        // Filter out OpenGL getProcAddress warnings. Loader to resolve
+        // all possible function/extension names at startup which causes
+        // emscripten to spam the console log with warnings.
+        if (
+          text.startsWith !== undefined &&
+          text.startsWith("bad name in getProcAddress:")
+        )
+          return;
+
+        if (config.stderrEnabled) console.log(text);
+      };
+
+    // Error handling: set status to "Exited", update crashed and
+    // exitCode according to exit type.
+    // Emscripten will typically call printErr with the error text
+    // as well. Note that emscripten may also throw exceptions from
+    // async callbacks. These should be handled in window.onerror by user code.
+    Module.onAbort =
+      Module.onAbort ||
+      function (text) {
+        pAPI.crashed = true;
+        pAPI.exitText = text;
+        setStatus("Exited");
+      };
+    Module.quit =
+      Module.quit ||
+      function (code, exception) {
+        if (exception.name == "ExitStatus") {
+          // Clean exit with code
+          pAPI.exitText = undefined;
+          pAPI.exitCode = code;
+        } else {
+          pAPI.exitText = exception.toString();
+          pAPI.crashed = true;
+        }
+        setStatus("Exited");
+      };
+
+    // Set environment variables
+    Module.preRun = Module.preRun || [];
+    Module.preRun.push(function () {
+      for (var [key, value] of Object.entries(config.environment)) {
+        ENV[key.toUpperCase()] = value;
+      }
+    });
+
+    Module.mainScriptUrlOrBlob = new Blob([emscriptenModuleSource], {
+      type: "text/javascript",
+    });
+
+    pAPI.exitCode = undefined;
+    pAPI.exitText = undefined;
+    pAPI.crashed = false;
+
+    // Finally evaluate the emscripten application script, which will
+    // reference the global Module object created above.
+    self.eval(emscriptenModuleSource); // ES5 indirect global scope eval
+  }
+
+  function setErrorContent() {
+    if (config.containerElements === undefined) {
+      if (config.showError !== undefined) config.showError(self.error);
+      return;
+    }
+
+    for (container of config.containerElements) {
+      var errorElement = config.showError(self.error, container);
+      container.appendChild(errorElement);
+    }
+  }
+
+  function setExitContent() {
+    if (pAPI.status != "Exited") return;
+    if (config.containerElements === undefined) {
+      if (config.showExit !== undefined)
+        config.showExit(pAPI.crashed, pAPI.exitCode);
+      return;
+    }
+    if (!pAPI.crashed) return;
+    for (container of config.containerElements) {
+      var loaderElement = config.showExit(
+        pAPI.crashed,
+        pAPI.exitCode,
+        container,
+      );
+      if (loaderElement !== undefined) container.appendChild(loaderElement);
+    }
+  }
+  var committedStatus = undefined;
+  function handleStatusChange() {
+    if (pAPI.status != "Loading" && committedStatus == pAPI.status) return;
+    committedStatus = pAPI.status;
+    if (pAPI.status == "Error") setErrorContent();
+    else if (pAPI.status == "Exited") setExitContent();
+    // Send status change notification
+    if (config.statusChanged) config.statusChanged(pAPI.status);
+  }
+
+  function setStatus(status) {
+    if (status != "Loading" && pAPI.status == status) return;
+    pAPI.status = status;
+    if (typeof window !== "undefined") {
+      window.setTimeout(function () {
+        handleStatusChange();
+      }, 0);
+    } else {
+      // We're in a Web Worker
+      setTimeout(function () {
+        handleStatusChange();
+      }, 0);
+    }
+  }
+  setStatus("Created");
+  return pAPI;
+}
+// above is the old loader.js, added to this file for simplicity
+// do not modify unless absolutely needed
+
+
+//cleans up the files because they arnt reliably deleted by closing tab or other things.
+//spam this function in any case where we are unsure of the virtual FS state.
 function fsCleanup() {
   const filesToDelete = ["/INPUT.ROM", "/patch.txt", "/OUTPUT.ROM"];
   for (const file of filesToDelete) {
